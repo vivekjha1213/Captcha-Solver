79855aaaa2f78af5b9a2f66391d4cedfc2cdcd9d
Vivek Kumar jha <67068290+vivekjha1213@users.noreply.github.com>
2023-04-05 18:40:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,21 +20,18 @@ app.post('/solve-captcha', async(req, res) => {
         res.status(400).send('Failed to fetch captcha');
         return;
     }
-
-
     let captchaImage = null;
     try {
         captchaImage = await sharp(captchaBuffer)
-            .resize(300)
+            .resize(200)
             .toBuffer();
     } catch (error) {
         console.error(error);
         res.status(500).send('Failed to process captcha image');
         return;
     }
-
-
     let captcha = null;
+
     try {
         captcha = await solveCaptcha(captchaImage);
     } catch (error) {
@@ -42,11 +39,9 @@ app.post('/solve-captcha', async(req, res) => {
         res.status(500).send('Failed to solve captcha');
         return;
     }
-
     // Return the solved captcha
     res.json({ captcha });
 });
-
 app.listen(8000, () => {
     console.log('Server started on port 8000');
 });
